66a99f9a3c8903fc347830b57225830a8c139ff7
haleck <haleckiydenisirl@gmail.com>
2025-01-22 17:16:40 +0700

    Добавлена настройка ViteConfig для импорта svg без постфикса "?react"

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr({
+    // svgr options: https://react-svgr.com/docs/options/
+    svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+    include: "**/*.svg",
+  })],
 });
