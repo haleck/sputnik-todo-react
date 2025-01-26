638e1203ad8eb310058eadc602dc8e302918d350
haleck <haleckiydenisirl@gmail.com>
2025-01-26 21:25:05 +0700

    tsconfig bugfix + inspect plugin vite installation

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [react(), svgr({
-    // svgr options: https://react-svgr.com/docs/options/
-    svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
-    include: "**/*.svg",
-  })],
+  plugins: [react(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+      include: "**/*.svg",
+    }),
+    Inspect()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
